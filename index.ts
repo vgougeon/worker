a243010759028866dd4ff3345f207d959ee486b7
Vicnent GOUGEON <gougeonvincent31@gmail.com>
2022-01-12 17:01:36 +0100

    open port

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+import shell from 'shelljs'
 
 app.get('/available-port', (req: any, res: any) => {
-    res.send('Not implemented')
+    const cmd = shell.exec('./scripts/open-port')
+    console.log(cmd)
+    res.send(cmd)
 })
 
 app.get('/start-container', (req: any, res: any) => {
