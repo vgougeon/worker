a99baea5a3748803a5be81b6dd4ab29ec79ebc49
JEscobar31 <jeremy.escobar@ynov.com>
2022-01-12 17:15:06 +0100

    salut

--- index.ts
+++ index.ts
@@ -4,9 +4,10 @@ const port = 3000
 import shell from 'shelljs'
 
 app.get('/available-port', (req: any, res: any) => {
-    const cmd = shell.exec('/opt/worker/scripts/available-port.sh 10000 10005')
+    const cmd = shell.exec('/bin/bash /opt/worker/scripts/available-port.sh 10000 10005')
+    const port = Number(cmd.stdout.replace('\n', ''))
     console.log(cmd)
-    res.send(cmd)
+    res.send({ availablePort: port })
 })
 
 app.get('/start-container', (req: any, res: any) => {
