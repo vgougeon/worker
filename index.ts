265a9b766d3b5784255a6cf1b05df508bf91cac3
Vicnent GOUGEON <gougeonvincent31@gmail.com>
2022-01-12 17:31:54 +0100

    url rewrite

--- index.ts
+++ index.ts
@@ -1,20 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+import { Request, Response } from 'express'
 import shell from 'shelljs'
 
-app.get('/available-port', (req: any, res: any) => {
+app.get('/worker/available-port', (req: Request, res: Response) => {
+    console.log(req.ip)
     const cmd = shell.exec('/bin/bash /opt/worker/scripts/available-port.sh 10000 10005')
     const port = Number(cmd.stdout.replace('\n', ''))
     console.log(cmd)
     res.send({ availablePort: port })
 })
 
-app.get('/start-container', (req: any, res: any) => {
+app.get('/worker/start-container', (req: any, res: any) => {
     res.send('Not implemented')
 })
 
-app.get('/projet-status', (req: any, res: any) => {
+app.get('/worker/projet-status', (req: any, res: any) => {
     res.send('Not implemented')
 })
 
