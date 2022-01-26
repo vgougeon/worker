6261a55ce7ec3413b02bcc6c3014d3d0752b2430
JEscobar31 <jeremy.escobar@ynov.com>
2022-01-26 11:50:05 +0100

    playbook nginx & fix script create_container

--- scripts/project-status.sh
+++ scripts/project-status.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-PROJECT_ID=$1
+project_id=$1
 
-exit 1
+if [ "$(docker ps -a | grep $project_id)" ]; then
+    stats=$(docker stats $project_id --no-stream --format "{{.ID}}_{{.Name}}_{{.CPUPerc}}_{{.MemUsage}}_{{.MemPerc}}")
+    port=$(docker port $project_id)
+    echo "${stats}_${port}"
+fi
+
+
