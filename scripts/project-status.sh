294561788c6e83e13683c4c26f0f92501932eac4
Vicnent GOUGEON <gougeonvincent31@gmail.com>
2022-03-30 11:10:02 +0200

    nomenclature

--- scripts/project-status.sh
+++ scripts/project-status.sh
@@ -2,8 +2,8 @@
 
 project_id=$1
 
-if [ "$(docker ps -a | grep ${project_id}_)" ]; then
-    stats=$(docker stats ${project_id}_ --no-stream --format "{{.ID}}_{{.Name}}_{{.CPUPerc}}_{{.MemUsage}}_{{.MemPerc}}")
-    port=$(docker port ${project_id}_)
+if [ "$(docker ps -a | grep ${project_id}-)" ]; then
+    stats=$(docker stats ${project_id}- --no-stream --format "{{.ID}}_{{.Name}}_{{.CPUPerc}}_{{.MemUsage}}_{{.MemPerc}}")
+    port=$(docker port ${project_id}-)
     echo "${stats}_${port}"
 fi
