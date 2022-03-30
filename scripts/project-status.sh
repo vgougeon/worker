fd40cb1f10171160d9a4c7737be6cc56b6476514
JEscobar31 <jeremy.escobar@ynov.com>
2022-03-30 10:36:51 +0200

    test

--- scripts/project-status.sh
+++ scripts/project-status.sh
@@ -3,7 +3,7 @@
 project_id=$1
 
 if [ "$(docker ps -a | grep ${project_id}_)" ]; then
-    stats=$(docker stats $project_id --no-stream --format "{{.ID}}_{{.Name}}_{{.CPUPerc}}_{{.MemUsage}}_{{.MemPerc}}")
-    port=$(docker port $project_id)
+    stats=$(docker stats ${project_id}_ --no-stream --format "{{.ID}}_{{.Name}}_{{.CPUPerc}}_{{.MemUsage}}_{{.MemPerc}}")
+    port=$(docker port ${project_id}_)
     echo "${stats}_${port}"
 fi
